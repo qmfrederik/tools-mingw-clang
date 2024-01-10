65963b054d8c71e974a4b5f1a788c9ded83724dd
Frederik Carlier <frederik.carlier@keysight.com>
2024-01-10 09:52:38 +0100

    Install and configure VS Code for local development

--- install.ps1
+++ install.ps1
@@ -10,3 +10,14 @@ C:\tools\msys64\usr\bin\bash --login -c 'pacman -S --needed --noconfirm ansible'
 
 # Run the provisioning playbook locally
 & 'C:\tools\msys64\usr\bin\bash.exe' --login -c 'ansible-playbook --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 /c/vagrant/playbook.yml'
+
+# Install VS Code so you can work in a Windows shell, using 'vagrant rdp';
+# this makes sense for a desktop environment only.
+choco install -y vscode
+
+# Configure VS Code to use the MSYS2 terminal by default
+New-Item -ItemType Directory -Path ${env:USERPROFILE}\.vscode-server\data\Machine\ -Force
+Copy-Item C:\vagrant\settings.json ${env:USERPROFILE}\.vscode-server\data\Machine\settings.json -Force
+
+New-Item -ItemType Directory -Path ${env:USERPROFILE}\AppData\Roaming\Code\User\ -Force
+Copy-Item C:\vagrant\settings.json ${env:USERPROFILE}\AppData\Roaming\Code\User\settings.json -Force
